725fe1f2a1da83bc54d8a68be6a45b1cb98bbe79
Pierre Larochelle <pierrebombay@gmail.com>
2013-01-30 08:42:05 -0800

    Rendering a local html file

--- WebsterEditor/WEViewController.m
+++ WebsterEditor/WEViewController.m
@@ -9,7 +9,7 @@
 #import "WEViewController.h"
 
 @interface WEViewController ()
-
+- (NSString*)html;
 @end
 
 @implementation WEViewController
@@ -17,10 +17,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSURL *url = [NSURL URLWithString:@"http://gaualofa.com"];
-    NSURLRequest *req = [NSURLRequest requestWithURL:url];
     
-    [self.webView loadRequest:req];
+    NSString *html = [self html];
+    NSURL *base = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"starter" ofType:@"html"]];
+    [self.webView loadHTMLString:html baseURL:base];
+}
+
+- (NSString*)html {
+    // do htmly stuff
+    NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"starter" ofType:@"html"];
+    NSString *bootstrapFile = [[NSBundle mainBundle] pathForResource:@"bootstrap.min" ofType:@"css"];
+    NSString *bootstrapResponsiveFile = [[NSBundle mainBundle] pathForResource:@"bootstrap-responsive.min" ofType:@"css"];
+    NSString *jQueryFile = [[NSBundle mainBundle] pathForResource:@"jquery-1.9.0.min" ofType:@"js"];
+    NSString *bootstrapJSFile = [[NSBundle mainBundle] pathForResource:@"bootstrap.min" ofType:@"js"];
+    
+    NSString *bootstrapJS = [NSString stringWithContentsOfFile:bootstrapJSFile
+                                                      encoding:NSUTF8StringEncoding
+                                                         error:nil];
+    NSString *jQuery = [NSString stringWithContentsOfFile:jQueryFile
+                                                 encoding:NSUTF8StringEncoding
+                                                    error:nil];
+    NSString *bootstrap = [NSString stringWithContentsOfFile:bootstrapFile
+                                                    encoding:NSUTF8StringEncoding
+                                                       error:nil];
+    NSString *bootstrapResponsive = [NSString stringWithContentsOfFile:bootstrapResponsiveFile
+                                                              encoding:NSUTF8StringEncoding
+                                                                 error:nil];
+    NSString *finalHTML = [NSString stringWithContentsOfFile:htmlFile
+                                                    encoding:NSUTF8StringEncoding
+                                                       error:nil];
+    
+    finalHTML = [finalHTML stringByReplacingOccurrencesOfString:@"[[bootstrapCSS]]"
+                                                     withString:[NSString stringWithFormat:@"<style type=\"text/css\">\n%@\n</style>", bootstrap]];
+    finalHTML = [finalHTML stringByReplacingOccurrencesOfString:@"[[bootstrapResponsiveCSS]]"
+                                                     withString:[NSString stringWithFormat:@"<style type=\"text/css\">\n%@\n</style>", bootstrapResponsive]];
+    finalHTML = [finalHTML stringByReplacingOccurrencesOfString:@"[[jqueryJS]]"
+                                                     withString:[NSString stringWithFormat:@"<script type=\"text/javascript\">\n%@\n</script>", jQuery]];
+    finalHTML = [finalHTML stringByReplacingOccurrencesOfString:@"[[bootstrapJS]]"
+                                                     withString:[NSString stringWithFormat:@"<script type=\"text/javascript\">\n%@\n</script>", bootstrapJS]];
+    
+    return finalHTML;
 }
 
 - (void)didReceiveMemoryWarning
