32e8488f75ff9f475983c55c3ec7d6993e4d1e9a
Pierre Larochelle <pierrebombay@gmail.com>
2013-02-11 21:37:20 -0800

    Adding default columns to row objects

--- WebsterEditor/client/dom.cljs
+++ WebsterEditor/client/dom.cljs
@@ -21,4 +21,4 @@
       (.setSingleRange (.getSelection js/rangy) r))))
 
 (defn new-row []
-  (js/$ "<div class=\"row-fluid selectable\"></div>"))
+  (js/$ "<div class=\"row-fluid selectable\"><div class=\"span4\"></div><div class=\"span8\"></div></div>"))
