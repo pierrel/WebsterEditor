a56bf3465324cd3bac6a434042f230ba22ebf9ed
Pierre Larochelle <pierrebombay@gmail.com>
2013-03-21 22:29:01 -0700

    Opening and closing settings panel and adding setting background

--- WebsterEditor/Classes/Controllers/WEGlobalSettingsViewController.m
+++ WebsterEditor/Classes/Controllers/WEGlobalSettingsViewController.m
@@ -10,7 +10,7 @@
 #import "WEViewController.h"
 
 @interface WEGlobalSettingsViewController ()
-
+-(void)openSettings:(UIGestureRecognizer*)openGesture;
 @end
 
 @implementation WEGlobalSettingsViewController
@@ -28,8 +28,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.settingsView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"dark_exa.png"]];
+    
     WEViewController *contentController = [[WEViewController alloc] initWithNibName:@"WEViewController_iPad" bundle:nil];
     [self.contentView addSubview:contentController.view];
+    
+    UISwipeGestureRecognizer *openGesture = [[UISwipeGestureRecognizer alloc] init];
+    openGesture.direction = UISwipeGestureRecognizerDirectionRight;
+    [openGesture addTarget:self action:@selector(openSettings:)];
+    [contentView addGestureRecognizer:openGesture];
+    
+    UISwipeGestureRecognizer *closeGesture = [[UISwipeGestureRecognizer alloc] init];
+    closeGesture.direction = UISwipeGestureRecognizerDirectionLeft;
+    [closeGesture addTarget:self action:@selector(closeSettings:)];
+    [contentView addGestureRecognizer:closeGesture];
 
 }
 
@@ -37,6 +49,29 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)openSettings:(UIGestureRecognizer *)openGesture {
+    if (openGesture.state == UIGestureRecognizerStateEnded && ![self isOpen]) {
+        [UIView animateWithDuration:0.1 animations:^{
+            CGSize size = self.contentView.frame.size;
+            self.contentView.frame = CGRectMake(self.settingsView.frame.size.width, 0, size.width, size.height);
+        }];
+    }
+}
+
+-(void)closeSettings:(UIGestureRecognizer *)openGesture {
+    if (openGesture.state == UIGestureRecognizerStateEnded && [self isOpen]) {
+        [UIView animateWithDuration:0.1 animations:^{
+            CGSize size = self.contentView.frame.size;
+            self.contentView.frame = CGRectMake(0, 0, size.width, size.height);
+        }];
+    }
+}
+
+
+-(BOOL)isOpen {
+    return self.contentView.frame.origin.x > 0;
 }
 
 @end
