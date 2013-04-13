4c6af9db6531e00ca7754623428fc15d71e2a5ff
Pierre Larochelle <pierrebombay@gmail.com>
2013-04-12 17:40:24 -0700

    First step at in-app repl support
    Creating compojure app and moving all client to compojure-accessible
    places

--- WebsterEditor/client/project.clj
+++ WebsterEditor/client/project.clj
@@ -3,9 +3,12 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojurescript "0.0-1552"]
                  [org.clojure/google-closure-library-third-party "0.0-2029"]
-                 [domina "1.0.2-SNAPSHOT"]]
-  :plugins [[lein-cljsbuild "0.3.0"]]
+                 [domina "1.0.2-SNAPSHOT"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-cljsbuild "0.3.0"]
+            [lein-ring "0.8.2"]]
+  :ring {:handler repl.handler/app}
   :cljsbuild {:builds [{:source-paths ["src/cljs"]
-                        :compiler {:output-to "src/js/development.js"
+                        :compiler {:output-to "resources/public/js/development.js"
                                    :optimizations :whitespace
                                    :pretty-print true}}]})
