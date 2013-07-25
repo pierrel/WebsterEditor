5572b1c590d27ddb0c70b8b700838a67b98983df
Pierre Larochelle <pierrebombay@gmail.com>
2013-07-25 08:10:04 -0700

    Hooking up page selection to handler

--- WebsterEditor/Classes/Controllers/WELinkViewController.m
+++ WebsterEditor/Classes/Controllers/WELinkViewController.m
@@ -155,13 +155,13 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
+    if (indexPath.section == 1) {
+        NSArray *pages = [self pages];
+        NSString *page = [pages objectAtIndex:indexPath.row];
+        NSLog(@"Selected %@", page);        
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
