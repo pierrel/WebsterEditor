c92f661b229803f80fb3bc400f22c6cb0a77c30f
Pierre Larochelle <pierrebombay@gmail.com>
2013-09-13 08:25:49 -0700

    Setting window tint color

--- WebsterEditor/WEAppDelegate.m
+++ WebsterEditor/WEAppDelegate.m
@@ -20,6 +20,7 @@
 {
     [self createDirectories];
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self.window setTintColor:[UIColor colorWithRed:0.933 green:0.408 blue:0.333 alpha:1.0]];
     
     // Override point for customization after application launch.
 //    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
