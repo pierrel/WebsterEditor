a01d06c299aca61c491fea0fb21d218b28201637
Pierre Larochelle <pierrebombay@gmail.com>
2013-10-29 08:08:48 -0700

    Updated clojure cljs, cljsbuild

--- WebsterEditor/client/project.clj
+++ WebsterEditor/client/project.clj
@@ -1,12 +1,12 @@
 (defproject webster "1"
   :description "in-browser code for the webster html editor"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1552"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1889"]
                  [org.clojure/google-closure-library-third-party "0.0-2029"]
                  [domina "1.0.2-SNAPSHOT"]
                  [compojure "1.1.5"]]
-  :plugins [[lein-cljsbuild "0.3.0"]
-            [lein-ring "0.8.2"]
+  :plugins [[lein-cljsbuild "0.3.4"]
+            [lein-ring "0.8.7"]
             [lein-lesscss "1.3-SNAPSHOT" :classpath "~/Documents/Source/lein-lesscss/src"]]
   :ring {:handler repl.handler/app}
   :cljsbuild {:builds [{:source-paths ["src/webster"]
