a4c9cdaee90d73e9e8880f34e402cda8c185ecb3
Pierre Larochelle <plarochelle@blurb.com>
2014-04-10 19:36:10 -0700

    Using clojars lein-lesscss

--- WebsterEditor/client/project.clj
+++ WebsterEditor/client/project.clj
@@ -7,7 +7,7 @@
                  [compojure "1.1.5"]]
   :plugins [[lein-cljsbuild "0.3.0"]
             [lein-ring "0.8.2"]
-            [lein-lesscss "1.3-SNAPSHOT" :classpath "~/Documents/Source/lein-lesscss/src"]]
+            [lein-lesscss "1.2"]]
   :ring {:handler repl.handler/app}
   :cljsbuild {:builds [{:source-paths ["src/webster"]
                         :compiler {:output-to "resources/public/js/development.js"
