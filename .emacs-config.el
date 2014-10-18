95281016ae280ea4fe2f0a995ed65d1210a88e42
Pierre Larochelle <pierrebombay@gmail.com>
2014-10-18 10:47:59 -0700

    Adding the webserver command

--- .emacs-config.el
+++ .emacs-config.el
@@ -2,6 +2,13 @@
     (progn
       (defun webster-repl ()
         (interactive)
+        (webster-server)
         (with-project-root
-            (run-lisp (concat default-directory "WebsterEditor/scripts/repl"))))))
+            (run-lisp (concat default-directory "WebsterEditor/scripts/repl"))))
+      (defun webster-server ()
+        (interactive)
+        (with-project-root
+            (start-process "webster server" "*Webster-server*" (concat default-directory "WebsterEditor/scripts/server"))))))
+
+
 
